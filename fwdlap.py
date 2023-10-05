3bfa2455ad219cc98385898312a5e373f1631393
Yixiao Chen <yixiaoc@princeton.edu>
2023-10-04 23:43:48 -0400

    fix license

--- fwdlap.py
+++ fwdlap.py
@@ -1,4 +1,4 @@
-# Copyright 2020 The JAX Authors.
+# Copyright 2023 Yixiao Chen.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,6 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# Part of this code take references from jet and jvp in jax
 
 from typing import Any, Callable, Sequence, Union
 
